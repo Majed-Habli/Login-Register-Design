adef62a2705c2cbec430cb9b8b0b5b0323dc2e99
majed <majedhabli2020@gmail.com>
2023-07-20 23:30:42 +0300

    fixed reading input functionallity..

--- front-end/javascript/scripts.js
+++ front-end/javascript/scripts.js
@@ -1,9 +1,15 @@
 // document.addEventListener("DOMContentLoaded", redirect);
-const login_email = document.getElementById("login_email");
-const login_pasword = document.getElementById("login_password").value;
 
 const login_button = document.getElementById("loginbtn").addEventListener("click",showMessage);
 
 function showMessage(){
-    console.log( login_email );
+    const login_email = document.getElementById("login_email").value;
+    const login_pasword = document.getElementById("login_password").value;
+
+    if ( login_email == ""  ||  login_pasword == "" ){
+        console.log("check empty fields");
+    }else {
+        // check if data in db
+    }
 }
+
