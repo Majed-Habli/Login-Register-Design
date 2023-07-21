fd6de7918de90e048b3d34ac659a462bac0f99f1
majed <majedhabli2020@gmail.com>
2023-07-21 05:49:30 +0300

    Modified the scripts,js file.

--- front-end/javascript/scripts.js
+++ front-end/javascript/scripts.js
@@ -1,4 +1,5 @@
   const login_button = document.getElementById("loginbtn");
+  const register_button = document.getElementById("registerbtn");
 
   async function getData() {
     const login_email = document.getElementById("login_email").value;
@@ -20,16 +21,48 @@
       
       console.log(data); 
       return data;
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      console.log(error);
       return null;
     }
+  }
 
+
+    async function postData() {
+    const user_name = document.getElementById("user_name").value;
+    const register_email = document.getElementById("register_email").value;
+    const user_password = document.getElementById("user_password").value;
+    const user_gender = document.getElementById("user_gender").value;
     
+    const register_data = { name : user_name,registered_email: register_email, password1: user_password, gender: user_gender };
+    const url = "http://localhost/Login-Register-Design/back-end/signup.php";
+    const body = JSON.stringify(register_data);
+    
+    try {
+      const data = await fetch(url, {
+        method: "post",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: body
+      }).then(response => response.json())
+      .then(data=>(localStorage.setItem("response" , JSON.stringify(data))));
+      
+      console.log(data); 
+      return data;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
   }
 
   login_button.addEventListener("click", function (event) {
     event.preventDefault(); 
     getData();
     window.location.href= "/front-end/javascript/dashboard.html"
+  });
+
+  register_button.addEventListener("click", function (e) {
+    e.preventDefault(); 
+    postData();
   });
